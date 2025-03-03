c16a563b303ed5fb2bb242d7c25dd968a6cbc7dd
G-0nE007 <167809283+G-0nE007@users.noreply.github.com>
2025-03-03 15:13:05 +0530

    Update add.java

--- add.java
+++ add.java
@@ -4,11 +4,12 @@ public class AddTwoNumbers {
         // Define two numbers to add
         int num1 = 10;
         int num2 = 20;
+        int num3 = 30;
 
         // Calculate the sum
-        int sum = num1 + num2;
+        int sum = num1 + num2 + num3;
 
         // Print the result
-        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+        System.out.println("The sum of " + num1 + " and " + num2 + " and " + num3 + " is: " + sum);
     }
 }
